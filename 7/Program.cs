b6586e549f2f997642bd6e7739b94edac857b662
Alona Saienko <saienko.alona@gmail.com>
2024-03-15 10:55:12 +0200

    fdfdfd

--- 7/Program.cs
+++ 7/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Варіант 1:
+Створіть клас ArrayManipulator, який має методи для роботи з масивами цілих чисел:
+Метод GenerateRandomArray(int length, int min, int max), 
+який створює та повертає новий масив заданої довжини з випадковими числами в діапазоні від min до max.
+Метод FindMax(int[] array), який знаходить та повертає найбільший елемент у масиві.
+Метод SortArray(int[] array), який сортує масив у зростаючому порядку.
+
+Після створення класу запустіть програму, яка створює масив, 
+знаходить найбільший елемент та сортує масив. 
+Виведіть початковий масив, знайдений максимум та відсортований масив на консоль.*/
+
+using System;
+
+static class ArrayManipulator
+{
+    static public int[] GenerateRandomArray(int length, int min, int max)
+    {
+        int[] array = GenerateRandomArray(length, min, max);
+        
+        return array;
+    }
+    static public int FindMax(int[] array)
+    {
+        int max = array.Max();
+        return max;
+    }
+    /*static public void Sort(int[] array)
+    {
+        int[] sortarray = new int[array.Length];
+        for (int i = 0; i<array.Length; i++)
+        {
+            sortarray = array.Where(array[i] > array[i+1]);
+        }
+    }*/
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = ArrayManipulator.GenerateRandomArray(10, 5, 10);
+        Console.WriteLine(ArrayManipulator.FindMax(array));
+    }
+}
--- 7/obj/Debug/net8.0/7.AssemblyInfo.cs
+++ 7/obj/Debug/net8.0/7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d4db7e3d687356e369fe813e40809e9c4f42e1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
